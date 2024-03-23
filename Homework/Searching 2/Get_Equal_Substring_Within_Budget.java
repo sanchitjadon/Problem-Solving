df88f29389f71e798140cf9db54b13c3513892ad
sanchitjadon <145696918+sanchitjadon@users.noreply.github.com>
2024-03-23 23:47:05 +0530

    Update Get_Equal_Substring_Within_Budget.java

--- Homework/Searching 2/Get_Equal_Substring_Within_Budget.java
+++ Homework/Searching 2/Get_Equal_Substring_Within_Budget.java
@@ -1,4 +1,6 @@
  // brute force Solution
+
+ //fails when substring found in middle for e.g  s=krrgw  t=zjxss      output =1    expected=2
     public static int equalSubstring(String s,String t,int maxCost){
          int i=0;
          while(i<s.length()){
@@ -13,3 +15,25 @@
          }
          return i;
     }
+
+
+// O(n) time complexity 
+// sliding window approach
+
+class Solution {
+    public int equalSubstring(String s, String t, int maxCost) {
+        int ans=-1;
+        int window=0;
+        int i=0;
+        int n=s.length();
+        for(int j=0;j<n;j++){
+            window=window+Math.abs(s.charAt(j)-t.charAt(j));
+            while(window>maxCost){
+                window=window-Math.abs(s.charAt(i)-t.charAt(i));
+                i++;
+            }
+            ans=Math.max(ans,j-i+1);
+        }
+        return ans;
+    }
+}
