6413fd73aa51d9248dba1c081dc8b4c44616af20
sanchitjadon <145696918+sanchitjadon@users.noreply.github.com>
2024-04-15 23:43:49 +0530

    Update Maximum XOR of two numbers in an array.java

--- Homework/Trie 1/Maximum xor of two numbers in an array.java
+++ Homework/Trie 1/Maximum xor of two numbers in an array.java
@@ -5,75 +5,41 @@ class trie{
     }
 }
 class Solution {
-    public void insert(String binary,trie root){
+    public void insert(int num,trie root){
         trie curr=root;
-        for(int i=0;i<binary.length();i++){
-            int index=binary.charAt(i)-'0';
-            if(curr.childs[index]==null){
-                curr.childs[index]=new trie();
+        for(int i=31;i>=0;i--){
+            int currBit=(num>>i)&1;
+            if(curr.childs[currBit]==null){
+                curr.childs[currBit]=new trie();
             }
-            curr=curr.childs[index];
+            curr=curr.childs[currBit];
         }
     }
-    public String getmaxXOR(String word,trie root){
+    public int getmaxXOR(int num,trie root){
         trie curr=root;
-        String str="";
-        for(int i=0;i<word.length();i++){
-            char idx=word.charAt(i);
-            if(idx=='0' && curr.childs['1'-'0']!=null){
-                str+="1";
-                curr=curr.childs['1'-'0'];
-            }
-            else if(idx=='1' && curr.childs['0'-'0']!=null){
-                str+="0";
-                curr=curr.childs['0'-'0'];
+        int xor=0;
+        for(int i=31;i>=0;i--){
+            int currBit=(num>>i)&1;
+            int flipBit=1-currBit;
+            if(curr.childs[flipBit]!=null){
+                xor=xor|(1<<i);
+                curr=curr.childs[flipBit];
             }
             else{
-                str+=String.valueOf(idx);
-                curr=curr.childs[idx-'0'];
+                curr=curr.childs[currBit];
             }
         }
-        return str;
-    }
-    public int xor(String binary){
-        int dec_value=0;
-        int base=1;
-        int len = binary.length();
-        for (int i = len - 1; i >= 0; i--) {
-            if (binary.charAt(i) == '1')
-                dec_value += base;
-            base = base * 2;
-        }
-        return dec_value;
+        return xor;
     }
     public int findMaximumXOR(int[] nums) {
-        String[]binary=new String[nums.length];
-        for(int i=0;i<nums.length;i++){
-            String s="";
-            for(int j=31;j>=0;j--){
-                int k=nums[i]>>j;
-                if((k&1)>0){
-                    s+="1";
-                }
-                else{
-                    s+="0";
-                }
-            }
-            binary[i]=s;
-        }
         trie root=new trie();
-        for(String key:binary){
-            insert(key,root);
-        }
+        for(int i=0;i<nums.length;i++){
+           insert(nums[i],root);
+        } 
         int max=Integer.MIN_VALUE;
-        for(String word:binary){
-            String str=getmaxXOR(word,root);
-            int a=xor(str);
-            int b=xor(word);
-            int ans=a^b;
-            if(ans>max){
-                max=ans;
-            }
+        for(int num:nums){
+            int xor=getmaxXOR(num,root);
+            max=Math.max(max,xor);
         }
         return max;
     }
