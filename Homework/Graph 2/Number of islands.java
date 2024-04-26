a7267b0c9b25c7df63edb4fb5a5e0b6a6219f54c
sanchitjadon <145696918+sanchitjadon@users.noreply.github.com>
2024-04-26 14:18:52 +0530

    Update Number of islands.java

--- Homework/Graph 2/Number of islands.java
+++ Homework/Graph 2/Number of islands.java
@@ -1,21 +1,27 @@
 class Solution {
-    public void dfs(char[][]grid,int m,int start,boolean []visited){
-        visited[start]=true;
-        for(int i=0;i<grid[0].length;i++){
-            if(grid[start][i]==1 && !visited[i]){
-                dfs(grid,m,i,visited);
+    public void dfs(char[][] grid,boolean[][]visited,int m,int n,int[]Drow,int[]Dcol,int row,int col){
+        visited[row][col]=true;
+        for(int i=0;i<4;i++){
+            int nr=row+Drow[i];
+            int nc=col+Dcol[i];
+            if(nr>=0 && nr<m && nc>=0 && nc<n && !visited[nr][nc] && grid[nr][nc]=='1'){
+                dfs(grid,visited,m,n,Drow,Dcol,nr,nc);
             }
         }
     }
     public int numIslands(char[][] grid) {
         int m=grid.length;
         int n=grid[0].length;
-        boolean[]visited=new boolean[m];
+        boolean[][]visited=new boolean[m][n];
+        int[]Drow={-1,0,1,0};
+        int[]Dcol={0,1,0,-1};
         int count=0;
         for(int i=0;i<m;i++){
-            if(!visited[i]){
-                dfs(grid,m,i,visited);
-                count++;
+            for(int j=0;j<n;j++){
+                if(grid[i][j]=='1' && !visited[i][j]){
+                    count++;
+                    dfs(grid,visited,m,n,Drow,Dcol,i,j);
+                }
             }
         }
         return count;
