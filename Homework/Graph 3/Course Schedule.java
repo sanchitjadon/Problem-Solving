f4b56f197ad167ef79a47613e2268987ee0b6afb
sanchitjadon <145696918+sanchitjadon@users.noreply.github.com>
2024-05-02 18:28:42 +0530

    Update Course Schedule.java

--- Homework/Graph 3/Course Schedule.java
+++ Homework/Graph 3/Course Schedule.java
@@ -1,39 +1,38 @@
 class Solution {
-    HashMap<Integer, Set<Integer>> adj = new HashMap<>();
-    Set<Integer> exploring = new HashSet<>();
-    Set<Integer> visited = new HashSet<>();
-  
-    public boolean canFinish(int numCourses, int[][] prerequisites) {        
-        for (int i = 0; i < numCourses; i++) {
-            adj.put(i, new HashSet());
+    public boolean dfs(int v,List<List<Integer>>adj,boolean[]vis,boolean[]pathVis,int src){
+        vis[src]=true;
+        pathVis[src]=true;
+        for(int x:adj.get(src)){
+            if(!vis[x]){
+                if(dfs(v,adj,vis,pathVis,x)){
+                    return true;
+                }
+            }
+            else if(pathVis[x]){
+                return true;
+            }
         }
-        for (int i = 0; i < prerequisites.length; i++) {
-            adj.get(prerequisites[i][1]).add(prerequisites[i][0]);
+        pathVis[src]=false;
+        return false;
+    }
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        int v=numCourses;
+        List<List<Integer>>adj=new ArrayList<>();
+        for(int i=0;i<v;i++){
+            adj.add(new ArrayList<>());
         }
-        for (int i = 0; i < numCourses; i++) {
-            if (!visited.contains(i)) {
-                boolean hasCycle = dfsDetectCycle(i);
-                if (hasCycle) 
-                  return false; 
+        for(int[]edge:prerequisites){
+            adj.get(edge[1]).add(edge[0]);
+        }
+        boolean[]vis=new boolean[v];
+        boolean[]pathVis=new boolean[v];
+        for(int i=0;i<v;i++){
+            if(!vis[i]){
+                if(dfs(v,adj,vis,pathVis,i)){
+                    return false;
+                }
             }
         }
         return true;
-    }
-    public boolean dfsDetectCycle(int n) {
-        if (visited.contains(n)) 
-          return false;
-        if (exploring.contains(n)) 
-          return true;
-        exploring.add(n);
-        for (int e: adj.get(n)) {
-            if (!visited.contains(e)) {
-                boolean hasCycle = dfsDetectCycle(e);
-                if (hasCycle)
-                  return true;
-            }
-        }
-        exploring.remove(n);
-        visited.add(n);
-        return false;
     }
 }
